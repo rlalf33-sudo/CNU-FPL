d18d140f860574d4c70f7558fa96ebce4f63995f
Sungmin Jeong <sungmin@jnu.ac.kr>
2026-08-18 11:52:39 +0900

    Update People and finalize website draft

--- src/data/members.js
+++ src/data/members.js
@@ -1,6 +1,7 @@
 import dohunPortrait from '../assets/people/graduate/dohun.jpg'
 import jeongPortrait from '../assets/people/professor/jeong.jpg'
 import yeeunPortrait from '../assets/people/undergraduate/Yeni.jpg'
+import youminPortrait from '../assets/people/undergraduate/Yuni.jpg'
 
 const members = {
   professor: {
@@ -23,7 +24,7 @@ const members = {
       name: 'Dohun Kim',
       position: 'M.S. Student',
       email: 'thsg9912@naver.com',
-      researchKeywords: ['Rice Processing', 'Baking', 'Rheology'],
+      researchKeywords: ['Cereal Processing', 'Rice', 'Baking', 'Rheology'],
       image: dohunPortrait,
     },
   ],
@@ -34,6 +35,13 @@ const members = {
       email: 'edc3355@naver.com',
       researchKeywords: ['Surimi Processing', 'Fish Cakes'],
       image: yeeunPortrait,
+    },
+    {
+      name: 'Youmin Hwang',
+      position: 'Undergraduate',
+      email: 'hwangyoumin4315@naver.com',
+      researchKeywords: ['Cereal processing', 'Wheat', 'Baking'],
+      image: youminPortrait,
     },
   ],
   alumni: [],
